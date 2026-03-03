eef8358c80f6213b226dd03ff21d41ff7391ef26
Fanxu Meng <60565778+fxmeng@users.noreply.github.com>
2026-03-03 10:38:04 +0800

    update

--- megatron_patch/data/multimodal_dataset_helper.py
+++ megatron_patch/data/multimodal_dataset_helper.py
@@ -195,166 +195,54 @@ class TaskEncoder(DefaultTaskEncoder[ChatMLSample, EncodedSample, EncodedBatch, 
         return flattened, np.array(thw_grids)
 
     def encode_chatml(self, sample: ChatMLSample) -> EncodedSample:
-        # TODO: modify get_visual_transform to add more augmentations
         factor = 2 * self.args.patch_size
         min_pixels = 4 * self.args.patch_size * 4 * self.args.patch_size
         max_pixels = self.args.patch_size * self.args.patch_size * 4 * 1280
-        # before feature extraction: type of img: <class 'PIL.Image.Image'>
         imgs = [get_visual_transform(img, factor, min_pixels, max_pixels)[0] for img in sample.imgs]
-        # after feature extraction: type of img: <class 'torch.Tensor'>
-        # [[<PIL.Image.Image image mode=RGB size=1300x876 at 0x7F8A3855AAE0>, <PIL.Image.Image image mode=RGB size=1300x876 at 0x7F8A3855AB70>]]
-        videos = [[get_visual_transform(frame, factor, min_pixels, max_pixels)[0] for frame in video] for video in sample.videos]
-        # NOTE: make n_frames even foreach video
-        for i, video in enumerate(videos):
-            videos[i] = video[:len(video) // 2 * 2]
-
-        audios = []
-        audio_lengths = []
-        audio_feature_attention_masks = []
-        for audio, sampling_rate in sample.audios:
-            audio_kwargs = {"sampling_rate": 16000, "padding": True, "truncation": False, "return_attention_mask": True,}
-            audio = audio.detach().numpy()
-
-            # NOTE: only mono audio is supported, treat with multi-audios if we need to train multi-channel audios
-            if audio.shape[0] > 1:
-                audio = audio[[0]]
-            audio_inputs = self.feature_extractor(audio, **audio_kwargs)
-
-            audio_inputs["feature_attention_mask"] = audio_inputs.pop(
-                "attention_mask"
-            )  # rename feature_attention_mask to prevent conflicts later on
-            audio_inputs["input_features"] = audio_inputs.pop(
-                "input_features"
-            )  # rename input_features to prevent conflicts later on
-
-            #type of feature_attention_mask: <class 'numpy.ndarray'>, shape of feature_attention_mask: (1, 1209)
-            feature_attention_mask = audio_inputs["feature_attention_mask"]
-            audio_feature_attention_masks.append(feature_attention_mask)
-            #print(f"type of feature_attention_mask: {type(feature_attention_mask)}, shape of feature_attention_mask: {feature_attention_mask.shape}")
-            # type of input_features: <class 'numpy.ndarray'>, shape of input_features: (1, 80, 1209)
-            # audio length: [158]
-            audios.append(audio_inputs["input_features"]) # (N, ?)
-            audio_lengths.append(_get_feat_extract_output_lengths(int(audio_inputs["feature_attention_mask"].sum(-1)))) # (N, )
-
-
-        # NOTE: flatten all images
-        # flattened_imgs[0] shape (4428, 1536), image_thw_grids shape (1, 3), image_thw_grids [[ 1 54 82]]
         flattened_imgs, image_thw_grids = self._flatten_visual_inputs(imgs, is_image=True)
-        # [torch.Size([3, 864, 1312]),torch.Size([3, 864, 1312])]
-        flattened_videos, video_thw_grids = self._flatten_visual_inputs(videos, is_image=False)
-
-        # NOTE: generate qwen2vl conversations
         conversation = json.loads(sample.conversation) if isinstance(sample.conversation, (str, bytes)) else sample.conversation
-        second_per_grid_ts = [1 / 2.0] * len(video_thw_grids)
-        assert 'conversations' in conversation
-        assert 'discrete_tokens' in conversation
-        assert 'second_per_grid_ts' in conversation
-        second_per_grid_ts = conversation.get('second_per_grid_ts', second_per_grid_ts)
-        second_per_grid_ts = [float(i) for i in second_per_grid_ts]
-        discrete_tokens = conversation['discrete_tokens']
-        conversation = conversation['conversations']
-        conversation = '\n'.join([conv['content'] for conv in conversation])
-        conversation = conversation.replace("<image>", "<|vision_start|><|image_pad|><|vision_end|>")
-        input_ids = self.tokenizer(conversation, padding='do_not_pad', return_tensors="np").input_ids[0]
-        target = input_ids.copy()
+        input_ids = np.array(conversation['tokens'])
         pad_token_id = self.tokenizer.pad_token_id
-        # NOTE: expand image_pad & video_pad & audio_pad
-        merge_length = self.merge_size**2
-        image_token_id, video_token_id, audio_token_id, first_vision_token_id = self.tokenizer.encode(['<|image_pad|>', '<|video_pad|>', '<|audio_pad|>', "<|vision_0|>"])
+
+
+        image_token_id = self.tokenizer.encode('<|image_pad|>')[0]
         image_token_indices = np.where(input_ids == image_token_id)[0]
-        assert len(image_token_indices) == len(image_thw_grids), f"With {len(image_thw_grids)} images in the sample, but {len(image_token_indices)} image placeholders!"
-        video_token_indices = np.where(input_ids == video_token_id)[0]
-        assert len(video_token_indices) == len(video_thw_grids), f"With {len(video_thw_grids)} images in the sample, but {len(video_token_indices)} video placeholders!"
-        audio_token_indices = np.where(input_ids == audio_token_id)[0]
-        assert len(audio_token_indices) == len(audio_lengths), f"With {len(audio_lengths)} audios in the sample, but {len(audio_token_indices)} audio placeholders!"
-        image_thw_grids, video_thw_grids, audio_lengths = (
-            np.array(image_thw_grids, dtype=np.int64), 
-            np.array(video_thw_grids, dtype=np.int64),
-            np.array(audio_lengths, dtype=np.int64)
-        )
-        num_discrete_tokens = 0
-        for sub in discrete_tokens:
-            num_discrete_tokens+=len(sub)
-        # (N, 3)
-        target_length = (
-            input_ids.shape[0] 
-            - image_thw_grids.shape[0] + image_thw_grids.prod(axis=-1).sum() // merge_length
-            - video_thw_grids.shape[0] + video_thw_grids.prod(axis=-1).sum() // merge_length
-            - audio_lengths.shape[0] + audio_lengths.sum()
-            + num_discrete_tokens
-        )
-        if target_length > self.seq_len:
-            raise InternalWarning(f"Long sequence with length {target_length} largger than {self.seq_len} found, dropped...")
-        final_input_ids = np.zeros(target_length, dtype=input_ids.dtype)
-        final_input_masks = final_input_ids.copy()
+        image_thw_grids = np.array(image_thw_grids, dtype=np.int64)
+        if len(input_ids) > self.seq_len:
+            raise InternalWarning(f"Long sequence with length {len(input_ids)} largger than {self.seq_len} found, dropped...")
 
-        image_idx, video_idx, audio_idx = 0, 0, 0
-        indices = np.sort(np.concatenate([image_token_indices, video_token_indices, audio_token_indices]))
-
-        # WARNING: we do not implement use_audio_in_video = True
-        cur_x, cur_y = 0, 0
-        for i, idx in enumerate(indices):
-            num_disc = len(discrete_tokens[i])
-            token_id = input_ids[idx]
-            if token_id == image_token_id:
-                size = image_thw_grids[image_idx].prod() // merge_length
-                image_idx += 1
-            elif token_id == video_token_id:
-                size = video_thw_grids[video_idx].prod() // merge_length
-                video_idx += 1
-            elif token_id == audio_token_id:
-                size = audio_lengths[audio_idx].prod()
-                audio_idx += 1
-            # NOTE:
-            # input_ids[cur_x:idx] -> final_input_ids[cur_y:cur_y + idx - cur_x]
-            # input_ids[idx] -> final_input_ids[cur_y + idx - cur_x: cur_y + idx - cur_x + size]
-            final_input_ids[cur_y: cur_y + idx - cur_x] = input_ids[cur_x:idx]
-            final_input_masks[cur_y: cur_y + idx - cur_x] = target[cur_x:idx]
-            cur_y += idx - cur_x
-            final_input_ids[cur_y: cur_y + size] = token_id
-            final_input_masks[cur_y: cur_y + size] = pad_token_id
-            cur_y += size
-            cur_x = idx + 1
-            final_input_ids[cur_y: cur_y+num_disc] = np.array(discrete_tokens[i])+first_vision_token_id
-            final_input_masks[cur_y: cur_y+num_disc] = np.array(discrete_tokens[i])+first_vision_token_id
-            cur_y +=num_disc
-
-        if cur_x < len(input_ids):
-            final_input_ids[cur_y:] = input_ids[cur_x:]
-            final_input_masks[cur_y:] = target[cur_x:]
-
-        target = np.roll(final_input_masks, shift=-1)
+            
+        input_masks = input_ids.copy()
+        input_masks[image_token_indices] = pad_token_id
+        target = np.roll(input_masks, shift=-1)
         target[-1] = pad_token_id
 
         if (target == pad_token_id).all():
             raise InternalWarning("Sample with all masked label, dropped.")
 
-        image_input_mask = final_input_ids == self.tokenizer.image_token_id
-        video_input_mask = final_input_ids == self.tokenizer.video_token_id
-        audio_input_mask = final_input_ids == self.tokenizer.audio_token_id
+        image_input_mask = input_ids == self.tokenizer.image_token_id
+        video_input_mask = input_ids == self.tokenizer.video_token_id
+        audio_input_mask = input_ids == self.tokenizer.audio_token_id
 
         # collect data
         return EncodedSample(
             __key__=sample.__key__,
             __subflavors__=sample.__subflavors__,
             imgs=flattened_imgs,
-            videos=flattened_videos,
-            audios=np.concatenate(audios, axis=0) if audios else None,
-
+            videos=[],
+            audios=None,
             image_thw_grids=image_thw_grids,
-            video_thw_grids=video_thw_grids,
-            audio_lengths=audio_lengths,
-            second_per_grid_ts = np.array(second_per_grid_ts, dtype=np.float32),
-
+            video_thw_grids=[],
+            audio_lengths=[],
+            second_per_grid_ts = np.array([], dtype=np.float32),
             image_input_mask=image_input_mask,
             video_input_mask=video_input_mask,
             audio_input_mask=audio_input_mask,
-            audio_feature_attention_mask=np.concatenate(audio_feature_attention_masks, axis=0) if audio_feature_attention_masks else None,
-
-            text=final_input_ids,
+            audio_feature_attention_mask= None,
+            text=input_ids,
             target=target,
         )
-    
+
     def batch(self, samples: List[EncodedSample]) -> EncodedBatch:
         # Stack images to [num_tiles, c, h, w]. If there are no images (text-only), then use a dummy image.
         imgs = [img for s in samples for img in s.imgs]
